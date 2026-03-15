36879cdd645adcca59b04703c62e1ee024cb4f41
Phạm Quang Trường <truongpham090325@gmail.com>
2026-03-15 11:10:59 +0700

    Hiển thị tour vào khối Giỏ hàng

--- controllers/client/cart.controller.js
+++ controllers/client/cart.controller.js
@@ -1,5 +1,61 @@
+const City = require("../../models/city.model");
+const Tour = require("../../models/tour.model");
+const moment = require("moment");
+
 module.exports.cart = (req, res) => {
   res.render("client/pages/cart", {
     pageTitle: "Giỏ hàng",
   });
+};
+
+module.exports.detail = async (req, res) => {
+  try {
+    const cart = req.body;
+    const cartDetail = [];
+
+    for (const item of cart) {
+      const tourInfo = await Tour.findOne({
+        _id: item.tourId,
+        deleted: false,
+        status: "active",
+      });
+
+      const cityInfo = await City.findOne({
+        _id: item.locationFrom,
+      });
+
+      if (tourInfo) {
+        cartDetail.push({
+          tourId: item.tourId,
+          locationFrom: item.locationFrom,
+          quantityAdult: item.quantityAdult,
+          quantityChildren: item.quantityChildren,
+          quantityBaby: item.quantityBaby,
+          avatar: tourInfo.avatar,
+          name: tourInfo.name,
+          departureDate: moment(tourInfo.departureDate).format("DD/MM/YYYY"),
+          cityName: cityInfo.name,
+          stockAdult: tourInfo.stockAdult,
+          stockChildren: tourInfo.stockChildren,
+          stockBaby: tourInfo.stockBaby,
+          priceNewAdult: tourInfo.priceNewAdult,
+          priceNewChildren: tourInfo.priceNewChildren,
+          priceNewBaby: tourInfo.priceNewBaby,
+          slug: tourInfo.slug,
+        });
+      }
+    }
+
+    res.json({
+      code: "success",
+      message: "Thành công!",
+      cart: cartDetail,
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      code: "error",
+      message: "Thất bại!",
+    });
+  }
 };
